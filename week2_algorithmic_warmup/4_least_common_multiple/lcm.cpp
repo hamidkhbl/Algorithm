1d19cc386630faebf0caf45df0ce49c8d3aff2e4
Hamid Khodabandeloo <hamid@Hamids-MBP.gateway.sasktel.net>
2020-10-13 22:13:43 -0600

    Add fast LCM

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.cpp
@@ -8,9 +8,46 @@ long long lcm_naive(int a, int b) {
   return (long long) a * b;
 }
 
+int fast_gcd(int a, int b)
+{
+  int large;
+  int small;
+  if(a == b)
+  {
+    return a;
+  }
+  else if(a > b)
+  {
+    large = a;
+    small = b;
+  }
+  else if(a < b)
+  {
+    large = b;
+    small = a;
+  }
+
+  int reminder;
+  reminder = large % small;
+
+  if(reminder == 0){
+    return small;
+  }
+  else
+  {
+    return fast_gcd(small, reminder);
+  }
+}
+
+long long fast_lcm(int a, int b)
+{
+  return (long long) a*b/fast_gcd(a,b);
+
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << lcm_naive(a, b) << std::endl;
+  std::cout << fast_lcm(a, b) << std::endl;
   return 0;
 }
