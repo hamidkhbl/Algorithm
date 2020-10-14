dfdd40094abf83c91e95394471a2296d9e42a665
Hamid Khodabandeloo <hamid@Hamids-MBP.gateway.sasktel.net>
2020-10-13 22:12:56 -0600

    Add fast GCD

--- week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
+++ week2_algorithmic_warmup/3_greatest_common_divisor/gcd.cpp
@@ -12,9 +12,41 @@ int gcd_naive(int a, int b) {
   return current_gcd;
 }
 
+int fast_gcd(int a, int b)
+{
+  int large;
+  int small;
+  if(a == b)
+  {
+    return a;
+  }
+  else if(a > b)
+  {
+    large = a;
+    small = b;
+  }
+  else if(a < b)
+  {
+    large = b;
+    small = a;
+  }
+
+  int reminder;
+  reminder = large % small;
+
+  if(reminder == 0){
+    return small;
+  }
+  else
+  {
+    return fast_gcd(small, reminder);
+  }
+}
+
 int main() {
   int a, b;
   std::cin >> a >> b;
-  std::cout << gcd_naive(a, b) << std::endl;
+  //std::cout << gcd_naive(a, b) << std::endl;
+  std::cout << fast_gcd(a, b) << std::endl;
   return 0;
 }
