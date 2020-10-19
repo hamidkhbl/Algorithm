8440d5d354822a7d6e420a8579bca1b6c9b3bc94
Hamid Khodabandeloo <hamid@Hamids-MBP.gateway.sasktel.net>
2020-10-19 14:53:04 -0600

    Add solution for fractional knapsack

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -3,12 +3,49 @@
 
 using std::vector;
 
+int max(int size,vector<double> arr)
+{
+  int max_index = 0;
+  for (int i = 0; i < size; i++)
+  {
+    if(arr[i] > arr[max_index]){
+      max_index = i;
+    }
+  }
+  return max_index;
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
+  int size = weights.size();
+  vector<double> ratios(size);
 
-  // write your code here
-
-  return value;
+   for (int i = 0; i < size ; i++)
+   {
+     ratios[i] = values[i]/weights[i];
+   }
+   
+   int ratio_max_index = 0;
+   while (capacity >= 0)
+   {
+      ratio_max_index = max(size, ratios);
+      ratios.erase(ratios.begin() + ratio_max_index);
+      if(weights[ratio_max_index] <= capacity)
+      {
+        value += values[ratio_max_index];
+        capacity-=weights[ratio_max_index];
+      }
+      else
+      {
+         value += values[ratio_max_index] * ((double) capacity/weights[ratio_max_index]);
+        // capacity = 0;
+        //std::cout<< values[ratio_max_index] <<','<< (double)capacity/weights[ratio_max_index] << ',' <<capacity<< std::endl;
+        break;
+      }
+            
+   } 
+   int round = (int)(value * 10000 + 0.5);
+  return double(round)/10000;
 }
 
 int main() {
@@ -24,6 +61,6 @@ int main() {
   double optimal_value = get_optimal_value(capacity, weights, values);
 
   std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  std::cout <<optimal_value << std::endl;
   return 0;
 }
