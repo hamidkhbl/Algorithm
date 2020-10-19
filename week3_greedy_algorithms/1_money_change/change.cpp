87ce0c176bba7c0023ec47d628695798bc4ebd13
Hamid Khodabandeloo <hamid@Hamids-MBP.gateway.sasktel.net>
2020-10-18 21:30:17 -0600

    Add money change solution

--- week3_greedy_algorithms/1_money_change/change.cpp
+++ week3_greedy_algorithms/1_money_change/change.cpp
@@ -2,6 +2,11 @@
 
 int get_change(int m) {
   //write your code here
+  int n = 0;
+  int n_10s = m / 10;
+  int n_5s = (m%10) / 5;
+  int n_1s = (m%10)%5;
+  n = n_10s + n_5s + n_1s ;
   return n;
 }
 
